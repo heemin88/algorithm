f09675ae3d23275d1eac3385c5595b9fcf608616
ParkHeeMin <gmlals9628@naver.com>
2023-01-26 17:41:38 +0900

    [Silver V] Title: 영화감독 숌, Time: 664 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,13 @@
+if __name__ == "__main__":
+    n = int(input())
+    num = 666
+    k=1
+    while True:
+        if k ==n:
+            print(num)
+            break
+
+        num +=1
+
+        if "666" in str(num):
+            k +=1
