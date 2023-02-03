f29528c16e03606d5cb588af4f0137d7a73e8ce8
ParkHeeMin <gmlals9628@naver.com>
2023-02-03 16:45:31 +0900

    [Silver III] Title: N과 M (1), Time: 164 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,15 @@
+arr= input().split()
+n= int(arr[0])
+m = int(arr[1])
+s =[]
+def dfs():
+    if len(s)==m:
+        print(' '.join(map(str,s)))
+        return
+    for i in range(1,n+1):
+        if i not in s:
+            s.append(i)
+            dfs()
+            s.pop()
+
+dfs()
