d78c4e8e3639e8eee41734c6572d344c1a8d1e38
ParkHeeMin <gmlals9628@naver.com>
2023-02-11 16:47:32 +0900

    [Silver III] Title: 1로 만들기, Time: 528 ms, Memory: 39068 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -0,0 +1,16 @@
+n = int(input())
+if n ==1 : print(0)
+else:
+    dp= [0]*(n+1)
+    dp[1] = 0
+    dp[2] = 1
+    for i in range(3,n+1):
+        dp[i] = dp[i - 1] + 1
+        if i % 3 == 0 :
+            if i%2 ==0:
+                dp[i]= min(dp[i - 1] + 1, dp[i // 3] + 1,dp[i // 2] + 1)
+            else: dp[i] =  min(dp[i - 1] + 1, dp[i // 3] + 1)
+        elif i%2 ==0:
+            dp[i] = min(dp[i - 1] + 1, dp[i // 2] + 1)
+            
+    print(dp[n])
