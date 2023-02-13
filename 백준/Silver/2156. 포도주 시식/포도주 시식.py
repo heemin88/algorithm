fec57f3eb551d577ae1ff6b3455cdd500085a5ef
ParkHeeMin <gmlals9628@naver.com>
2023-02-14 08:36:35 +0900

    [Silver I] Title: 포도주 시식, Time: 432 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.py
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -1,13 +1,11 @@
-n=int(input())
-array=[0]*10000
-for i in range(n):
-  array[i]=int(input())
-
-d=[0]*10000
-d[0]=array[0]
-d[1]=array[0]+array[1]
-d[2]=max(array[2]+array[0], array[2]+array[1], d[1])
-for i in range(3,n):
-  d[i]=max(array[i]+d[i-2], array[i]+array[i-1]+d[i-3], d[i-1])
-
-print(max(d))
+# 동적계획법
+n = int(input())
+num= [int(input()) for _ in range(n)]
+dp= [0]*(n+1)
+dp[1] = num[0]
+if n==1 : print(dp[1])
+else:
+    dp[2] = num[1] +dp[1]
+    for i in range(3,n+1):
+        dp[i] = max(num[i-1]+dp[i-2],num[i-1]+num[i-2]+dp[i-3],dp[i-1])
+    print(max(dp))
