c111dcb52d4234fcd5c82e8d992d4e22f96d0251
ParkHeeMin <gmlals9628@naver.com>
2023-03-17 12:36:13 +0900

    [unrated] Title: 대충 만든 자판, Time: 1.48 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.py
@@ -0,0 +1,22 @@
+def solution(keymap, targets):
+    answer = []
+    n = len(keymap)
+    dict ={}
+    for i in range(n):
+        temp=list(keymap[i])
+        for j in range(len(temp)):
+            if dict.get(temp[j],0) ==0:
+                dict[temp[j]] = j+1
+            else:
+                dict[temp[j]] = min(dict[temp[j]], j+1)
+    for i in range(len(targets)):
+        input = list(targets[i])
+        cnt = 0
+        for j in range(len(input)):
+            if input[j] not in dict:
+                cnt = -1
+                break
+            else:
+                cnt +=dict[input[j]]
+        answer.append(cnt)
+    return answer
