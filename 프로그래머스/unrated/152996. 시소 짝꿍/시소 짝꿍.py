f4a88c72f38452dfc19419bb6da418f36c0d2c2b
Park HeeMin <gmlals9628@naver.com>
2023-04-20 10:53:30 +0900

    [unrated] Title: 시소 짝꿍, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/152996. 시소 짝꿍/시소 짝꿍.py
+++ 프로그래머스/unrated/152996. 시소 짝꿍/시소 짝꿍.py
@@ -1,18 +1,18 @@
 def solution(weights): #시소 짝꿍 : 탑승한 사람의 무게 == 시소 축과 좌선간의 거리의 곱
     answer = 0
-    d = [2,3,4]
-    dict ={}
-    for w in weights:
-        if dict.get(w,0) == 0:
-            dict[w] = 1
-        else:
-            dict[w] +=1
-    for w in weights:
-        if w %2 ==0 : answer += dict.get(w*3//2,0)
-        if w%3 == 0 : answer += dict.get(w*4//3,0)
-        answer += dict.get(w*2,0)
-    for w in weights:
-        dict[w] -=1
-        answer += dict[w]
+    dict={}
+    for i in weights:
+        if dict.get(i,0)==0:
+            dict[i]=1
+        else : dict[i]+=1
+    for i in weights:
+        if i%2 ==0:
+            answer += dict.get(i*3//2,0)
+        if i%3 ==0:
+            answer += dict.get(i*4//3,0)
+        answer += dict.get(i*2,0)
+    for i in weights:
+        dict[i]-=1
+        answer += dict[i]
     
     return answer
