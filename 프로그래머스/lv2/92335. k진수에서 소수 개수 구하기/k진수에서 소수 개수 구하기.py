04af913662f0ded11dc25947659dbb7e9a0aec47
Park HeeMin <gmlals9628@naver.com>
2023-05-15 15:58:07 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
@@ -0,0 +1,27 @@
+import re
+import math
+def int2base(num,k):
+    res = ''
+    while num>0:
+        num , mod = divmod(num,k)
+        res+=str(mod)
+    return res[::-1]
+def is_sosu(num):
+    if num <=1 : return False
+    for i in range(2,int(num**(0.5))+1):
+        if num%i == 0 :
+            return False
+    return True
+
+def solution(n, k):
+    #소수는 각 자릿수에 0을 포함하지 않는 소수
+    answer = 0 #n을 k진수로 바꿨을 때, 변환된 수 안에서 찾을 수 있는 소수의 개수 
+    n = int2base(n,k)
+    result =''
+    loc = -1 #0의 마지막 위치 
+    num = n.split('0')
+    for i in num:
+        if i != '' and is_sosu(int(i)):
+            answer +=1
+        
+    return answer
