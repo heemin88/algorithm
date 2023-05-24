534d9e414392621c16e99e14c0b76c9dbc9559ad
Park HeeMin <gmlals9628@naver.com>
2023-05-24 17:21:16 +0900

    [unrated] Title: 연속된 부분 수열의 합, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
+++ 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
@@ -1,27 +1,19 @@
-def solution(sequence, k):
-    answer = []
-    right = len(sequence)-1
-    left =right
-    result =sequence[right]
-    length =1000001
-    while left>=0:
-        if result > k: # 값이 더 커졌을 때, 다시 초기화 
-            result -=sequence[right]
-            right -=1
-        if result == k:# 값이 같을 때, 저장된 길이보다 짧으면 값 넣기
-            if length >= right-left:
-                result -= sequence[right]
-                answer.append([left,right])
-                length = right-left
-                right -=1
-                
-        left -=1
-        result += sequence[left]
-    num_min = length
-    final =[10000,10000]
-    
-    answer.sort()
-    for num,num2 in answer:
-        if num2-num == length:
-            return [num,num2]
-    
+def solution(sequence, k): #수열 정수 배열 , 부분수열의 합
+    answer = []#부분수열의 시작 인덱스와 마지막 인덱스를 담아 리턴
+    left =0
+    right =-1
+    now = 0
+    while True:
+        if now <k:
+            right +=1
+            if right >= len(sequence) : break
+            now += sequence [right]
+        else:
+            now -= sequence[left]
+            if left >= len(sequence): break
+            left+=1
+        if now == k:
+            answer.append([left,right]) 
+        
+    answer.sort(key = lambda x : (x[1]-x[0],x[0]))
+    return answer[0]
